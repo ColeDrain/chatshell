e2c45cfc26054f25db9dcae84935c3dabe568c79
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2023-03-24 19:49:49 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# ChatShell
+
+ChatShell is a CLI tool that helps you find shell commands for specific tasks. It uses OpenAI's GPT-3.5 language model to generate commands based on user input.
+
+## Installation
+
+To use ChatShell, you will need to have Golang installed on your machine. Once you have installed Golang, you can install ChatShell using the following command:
+
+go get github.com/yourusername/chatshell
+
+## Configuration
+
+ChatShell reads your OpenAI API key from a JSON configuration file named config.json. By default, it looks for this file in the $HOME/.chatshell/ directory.
+
+Here's an example of what your configuration file should look like:
+
+`{
+  "OPENAI_AUTH_TOKEN": "your_api_key_here"
+}`
+
+## Usage
+
+To use ChatShell, simply run the ask command followed by your query. For example:
+
+`bash
+chatshell ask "How do I list all files in a directory?"`
+
+
+This will return a shell command that you can use to list all files in a directory.
+
+You can also enter chat mode by using the -c flag:
+
+
+chatshell ask -c "Hello, how are you?"
+
+
+This will enter chat mode and allow you to have a normal conversation with ChatGPT.
+
+## Releases
+
+Releases of ChatShell will be generated periodically and added to the repository. These releases will be available for download in the Releases section of this repository. To use a release, simply download the binary for your operating system and place it in a directory that is in your system's PATH.
+
+## Contributing
+
+If you encounter any bugs or issues with ChatShell, please feel free to create an issue in the repository. Pull requests are also welcome if you would like to contribute to the development of ChatShell.
---
+++ cmd/chat.go
@@ -0,0 +1,83 @@
+/*
+Copyright © 2023 Ugochukwu Onyebuchi <pyvinci@gmail.com>
+*/
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	openai "github.com/sashabaranov/go-openai"
+)
+
+// askCmd represents the ask command
+var askCmd = &cobra.Command{
+	Use:   "ask [text]",
+	Short: "ask ChatGPT",
+	Long:  `ask is used to ask for shell commands for a particular task`,
+	Args:  cobra.ExactArgs(1),
+	Run:   runAsk,
+}
+var authToken string
+
+func init() {
+	rootCmd.AddCommand(askCmd)
+
+	askCmd.PersistentFlags().BoolP("chat", "c", false, "Chat with ChatGPT")
+
+	// Read the auth token from a configuration file
+	viper.SetConfigName("config")
+	viper.AddConfigPath("$HOME/.chatshell")
+	viper.SetEnvPrefix("OPENAI")
+	viper.AutomaticEnv()
+
+
+	if err := viper.ReadInConfig(); err == nil {
+		authToken = viper.GetString("OPENAI_AUTH_TOKEN")
+	} else {
+		fmt.Printf("Error reading configuration file: %v\n", err)
+	}
+
+	if authToken == "" {
+		fmt.Println("Error: OPENAI_AUTH_TOKEN environment variable not set")
+		os.Exit(1)
+	}
+}
+
+func runAsk(cmd *cobra.Command, args []string) {
+	client := openai.NewClient(authToken)
+	osType := runtime.GOOS
+	shell := os.Getenv("SHELL")
+	content := ""
+
+	chatMode, _ := cmd.Flags().GetBool("chat")
+
+	if !chatMode {
+        content = fmt.Sprintf(`You are a very helpful shell assistant that gives users only shell commands to achieve a task, just give out only the shell command(s), and nothing else, no preamble, greetings or explanation please, just the shell command. When you can't find a command for a query/prompt/greeting respond strictly with "Sorry I can't seem to find a command for that". Start now: "%v in %v os using %v shell"`, args[0], osType, shell)
+    } else {
+        content = args[0]
+    }
+
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role: openai.ChatMessageRoleUser,
+					Content: content,
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
